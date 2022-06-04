bf3473f5d7af3743818766167a4632239c84d7b5
Shaun <shaun.tung@gmail.com>
2022-06-05 04:02:58 +0800

    correct delete many script with console to know when it runs

--- cronjobs/scheduledScripts.js
+++ cronjobs/scheduledScripts.js
@@ -2,7 +2,8 @@ const Url = require("../models/Url");
 
 async function deleteExpiredEntries() {
   try {
-    await Url.deleteMany({ inactiveDate: { $lte: new Date() } });
+    await Url.deleteMany({ inactiveDate: { $gte: new Date() } });
+    console.log("deleted many");
   } catch (err) {
     console.log({ err });
   }
